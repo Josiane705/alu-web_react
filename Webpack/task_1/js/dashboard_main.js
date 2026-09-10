f6d18390535398d4c29b77ff0dabf92110c68261
Josiane705 <j.mukeshima@alustudent.com>
2026-09-10 08:02:43 +0000

    Fix build script and use _.debounce per checker requirements

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import { debounce } from 'lodash';
+import _ from 'lodash';
 
 $(document).ready(() => {
   $('body').append('<p>Holberton Dashboard</p>');
@@ -15,5 +15,5 @@ $(document).ready(() => {
     $('#count').text(`${count} clicks on the button`);
   }
 
-  $('#get-started').on('click', debounce(updateCounter, 300));
+  $('#get-started').on('click', _.debounce(updateCounter, 300));
 });
